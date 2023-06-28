6adfbb7802ae2d8f1070f1f5c6485acbe41ebb6b
BigJT <johntrihy@gmail.com>
2023-06-28 18:52:05 +0100

    Update jbox.tf

--- Sec09/Lab9.4/jbox.tf
+++ Sec09/Lab9.4/jbox.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "jbox-rg" {
 }
 
 module "jbox-vm" {
-  source = "../../modules/compute"
+  source = "../modules/compute"
   vm-name = "${var.env}-Jbox"
   subnet_id = module.fe-vnet.vnet_subnets[1]
   location = azurerm_resource_group.jbox-rg.location
